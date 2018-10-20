0a231584aaf7c49f3e42ad21f8e1fdf483c627c9
Kigenyi Phillip <codephillip@gmail.com>
2018-10-20 12:07:06 +0300

    added tests for adding and getting product

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from models import Product
 
 app = Flask(__name__)
 
 products = []
-products.append(Product(1, 'Water', 4))
 
 
 @app.route('/')
@@ -16,9 +15,24 @@ def hello_world():
 @app.route('/products', methods=['GET'])
 def get_all_products():
     json_products = []
+    print(products)
     for product in products:
         json_products.append(product.to_json())
-    return jsonify({'products': json_products})
+
+    print(json_products)
+    print(len(json_products))
+    if len(json_products) < 1:
+        return jsonify({'message': 'There are no products'}), 400
+    return jsonify({'products': json_products}), 200
+
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    print(data)
+    product = Product(data['id'], data['name'], data['price'])
+    products.append(product)
+    return jsonify({'products': [product.to_json()]}), 201
 
 
 if __name__ == '__main__':
--- tests/test_products.py
+++ tests/test_products.py
@@ -8,10 +8,31 @@ class TestProducts(unittest.TestCase):
     def setUp(self):
         self.app_tester = app.test_client()
 
-    def test_dictionary_response(self):
-        response = self.app_tester.get('http://127.0.0.1:5000/products')
+    def test_all_products(self):
+        input_data = {'id': 1, 'name': 'Phone', 'price': 200000}
+        self.app_tester.post('/products', json=input_data)
+        input_data = {'id': 2, 'name': 'Super', 'price': 30000}
+        self.app_tester.post('/products', json=input_data)
+
+        response = self.app_tester.get('/products')
         print(response)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data['products']), 2)
+        self.assertEqual(data['products'][0]['id'], 1)
+        self.assertEqual(data['products'][0]['name'], 'Phone Number')
+
+    def test_all_products_when_empty(self):
+        response = self.app_tester.get('/products')
+        print(response)
+        data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
+        self.assertIn('no products', data['message'])
+
+    def test_add_products(self):
+        input_data = {'id': 1, 'name': 'Phone', 'price': 2}
+        response = self.app_tester.post('/products', json=input_data)
+        self.assertEqual(response.status_code, 201)
 
 
 if __name__ == '__main__':
